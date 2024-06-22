30611e3c2e67bc016a932328c8c92e4e5f7a5ff2
asdfblarg <david82794@yahoo.com>
2024-06-21 22:52:53 -0400

    clean up about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,30 @@
-<p>Testing stuff out</p>
+<script>
+	import { goto } from '$app/navigation';
+
+	function handleClick() {
+		goto('/vatoto');
+	}
+</script>
+
+<div class="content-background">
+	<div class="content">
+		<div class="text">Just testing stuff out</div>
+	</div>
+</div>
+
+<style>
+	.content-background {
+        background-color: white;
+		padding-top: 5em;
+		padding-bottom: 25em;
+		margin: 5em;
+		display: flex;
+		justify-content: center;
+	}
+	.text {
+		/* text-align: center; */
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: xx-large;
+		padding: 2em;
+	}
+</style>
