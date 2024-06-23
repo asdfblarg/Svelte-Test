55883e96721231280fad4956d9aa63c309dac1d7
asdfblarg <david82794@yahoo.com>
2024-06-23 12:52:37 -0400

    update logout button css

--- src/routes/(authed)/+layout.svelte
+++ src/routes/(authed)/+layout.svelte
@@ -12,8 +12,23 @@
 
 <style>
 	form {
-		position: fixed;
-		top: 1.5em;
+		position: absolute;
+		top: 1em;
 		right: 1em;
+	}
+	button {
+		max-width: fit-content;
+		margin-left: auto;
+		margin-right: auto;
+		background-color: transparent;
+		border: 1px solid;
+		padding: 0.5em;
+		text-align: center;
+		font-size: 1em;
+	}
+	button:hover {
+		background-color: rgb(84, 120, 196);
+		border-color: black;
+		color: rgb(216, 226, 193);
 	}
 </style>
